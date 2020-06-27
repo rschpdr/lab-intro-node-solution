e87cc3d5553a19e41153848dba7f8134e0d62cec
rschpdr <pedro.resch@gmail.com>
2020-06-26 21:15:43 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+class SortedList {
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+
+  add(item) {
+    if (item !== undefined) {
+      this.items.push(item);
+      this.length++;
+      this.items.sort((a, b) => a - b);
+    }
+    return this.items;
+  }
+
+  get(pos) {
+    if (!this.length || pos > this.length) {
+      throw new Error("OutOfBounds");
+    }
+
+    return this.items[pos];
+  }
+
+  max() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return this.items[this.length - 1];
+  }
+
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return Math.min(...this.items);
+  }
+
+  sum() {
+    return this.items.reduce((acc, val) => acc + val, 0);
+  }
+
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return this.sum() / this.length;
+  }
+}
+
+module.exports = SortedList;
